890728c467239c07633703e7ef3d6f861ce53b62
JParedesArtica <126907318+JParedesArtica@users.noreply.github.com>
2023-06-12 22:02:25 +0000

    Pending changes exported from your codespace

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,6 +1,8 @@
 // Your code goes here:
-
+function renderPerson(name,birth,color,age,sex){
+  return name + " is a " + age + " years old " + sex + " born in " + birth + " with " + color + " eyes "
+}
 
 
   ///*** Do not edit below this line ****/
-  console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+  console.log(renderPerson('Katherine', '05/22/1983', 'green', 23, 'male'));
